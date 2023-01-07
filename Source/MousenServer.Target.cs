c90d39e9929fefb393232104557f57623abd4522
riverdigger <niallminer@gmail.com>
2023-01-06 17:57:03 -0800

    Attempt for server packaging and new sword tracing (that doesn't work)

---
+++ Source/MousenServer.Target.cs
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+using System;
+
+[SupportedPlatforms(UnrealPlatformClass.Server)]
+public class MousenServerTarget : TargetRules
+{
+    public MousenServerTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Server;
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+        ExtraModuleNames.Add("Mousen");
+    }
+}
